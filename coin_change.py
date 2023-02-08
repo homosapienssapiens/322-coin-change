33a5b6a5ff1cbce188ac020b7cd389b644cf9f24
Miguel Angel Solis Orozco <82777113+homosapienssapiens@users.noreply.github.com>
2023-02-08 14:47:00 -0500

    Code coments
    Comented the code for the coin change challenge.

--- coin_change.py
+++ coin_change.py
@@ -7,10 +7,19 @@ Created on Wed Feb  8 10:43:09 2023
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        arr = [float('inf')] * (amount + 1)
-        arr[0] = 0
-        for i in range(1, amount + 1):
-            for coin in coins:
-                if i >= coin:
-                    arr[i] = min(arr[i], arr[i - coin] + 1)
-        return arr[amount] if arr[amount] != float('inf') else -1
+    # Initialize an array with float('inf') values, except for the first element which is set to 0
+    arr = [float('inf')] * (amount + 1)
+    arr[0] = 0
+
+    # Loop through the possible amounts from 1 to amount
+    for i in range(1, amount + 1):
+        # Loop through each coin
+        for coin in coins:
+            # If the current amount is greater than or equal to the coin value
+            if i >= coin:
+                # Update the minimum number of coins needed to make the current amount
+                arr[i] = min(arr[i], arr[i - coin] + 1)
+
+    # Return the minimum number of coins needed to make amount
+    # If arr[amount] is still equal to float('inf'), it means there's no solution and we return -1
+    return arr[amount] if arr[amount] != float('inf') else -1
